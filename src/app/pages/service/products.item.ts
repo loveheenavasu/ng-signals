a823ce5e56358e6c894bf83ed5bfc5800fa3967a
loveheenavasu <trivediheenalove@gmail.com>
2024-06-10 16:20:46 +0530

    fix:price

--- src/app/pages/service/products.item.ts
+++ src/app/pages/service/products.item.ts
@@ -8,19 +8,19 @@ export const products:any[]=[
     {
         id: 2,
         name: 'Sony Headphone',
-        price: 23.99,
+        price: 23,
         imageUrl: 'https://www.sony.co.in/image/dd18cf93606d238305a733d336c45537?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF'
     },
     {
         id: 3,
         name: 'Macbook Air',
-        price: 649.00,
+        price: 649,
         imageUrl: 'https://atlas-content-cdn.pixelsquid.com/stock-images/apple-macbook-air-silver-laptop-XleKRJ3-600.jpg'
     },
     {
         id: 4,
         name: 'Nikon DSLR',
-        price: 250.00,
+        price: 250,
         imageUrl: 'https://i.pinimg.com/originals/6d/fc/da/6dfcda78e6445711dde808fa88afb22b.png'
     }
 ]
